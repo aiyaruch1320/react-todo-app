e315e47d19b9aca6427017cb9f6c574905d2a321
Aiyaruch <aiyaruch1320@hotmail.com>
2022-12-29 13:05:22 +0700

    mock service

---
+++ src/service/todo.service.ts
@@ -0,0 +1,33 @@
+import { ITodo } from "../interface/todo";
+import { ITodoRequest } from "./../interface/todo";
+
+export const getTodoList = async () => {
+  const listItems: ITodo[] = [
+    {
+      id: "1",
+      content: "Study Spanish",
+      completed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      order: 1,
+    },
+    {
+      id: "2",
+      content: "Workout",
+      completed: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      order: 2,
+    },
+  ];
+  return listItems;
+};
+
+export const createTodo = async (todo: ITodoRequest) => {
+  return {
+    ...todo,
+    completed: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+};
