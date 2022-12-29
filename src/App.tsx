1c3d793a30a8c9726189a121f360af1a715a5a1d
Aiyaruch <aiyaruch1320@hotmail.com>
2022-12-29 14:17:38 +0700

    add sort, remove, add Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,69 @@
 import { useEffect, useState } from "react";
+import { DropResult } from "react-beautiful-dnd";
 import "./App.css";
 import { TodoList } from "./components/ItemList";
 import { TextInput } from "./components/TextInput";
 import { ITodo } from "./interface/todo";
-import { getTodoList } from "./service/todo.service";
+import {
+  createTodo,
+  deleteTodo,
+  getTodoList,
+  sortTodoList,
+} from "./service/todo.service";
 
 function App() {
-  const [todo, setTodo] = useState<ITodo[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
   useEffect(() => {
-    getTodoList().then((res) => setTodo(res));
+    loadTodos();
   }, []);
+
+  const loadTodos = async () => {
+    try {
+      const todoList = await getTodoList();
+      setTodos(todoList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addTodo = async (todo: ITodo) => {
+    try {
+      await createTodo(todo);
+      setTodos([...todos, todo]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const sortTodos = async (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+    const newTodos = [...todos];
+    const [removed] = newTodos.splice(result.source.index, 1);
+    newTodos.splice(result.destination.index, 0, removed);
+    try {
+      await sortTodoList(newTodos);
+      setTodos(newTodos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removeTodo = async (id: string) => {
+    try {
+      await deleteTodo(id);
+      setTodos(todos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Drag and Drop</h1>
-      <TextInput todo={todo} setTodo={setTodo} />
-      <TodoList todo={todo} setTodo={setTodo} />
+      <TextInput todos={todos} addTodo={addTodo} />
+      <TodoList sortTodos={sortTodos} todos={todos} removeTodo={removeTodo} />
     </div>
   );
 }
