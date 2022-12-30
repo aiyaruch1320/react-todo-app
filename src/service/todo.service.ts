7338a6da9a89914a0904dca0df79fdf3ef9a5c7d
Aiyaruch <aiyaruch1320@hotmail.com>
2022-12-30 22:29:20 +0700

    add /api in path

--- src/service/todo.service.ts
+++ src/service/todo.service.ts
@@ -3,19 +3,19 @@ import { ITodo } from "../interface/todo";
 import { ITodoRequest } from "./../interface/todo";
 
 export const getTodoLists = async () => {
-  return httpClient.get<ITodo[]>("/todos").then((res) => res.data);
+  return httpClient.get<ITodo[]>("/api/todos").then((res) => res.data);
 };
 
 export const createTodo = async (todo: ITodoRequest) => {
-  return httpClient.post<ITodo>("/todos", todo).then((res) => res.data);
+  return httpClient.post<ITodo>("/api/todos", todo).then((res) => res.data);
 };
 
 export const sortTodoLists = async (todoList: ITodo[]) => {
   return httpClient
-    .put<ITodo[]>("/todos/sort", todoList)
+    .put<ITodo[]>("/api/todos/sort", todoList)
     .then((res) => res.data);
 };
 
 export const deleteTodo = async (id: string) => {
-  return httpClient.delete<void>(`/todos/${id}`);
+  return httpClient.delete<void>(`/api/todos/${id}`);
 };
